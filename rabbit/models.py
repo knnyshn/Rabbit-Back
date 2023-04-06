eae68ee626d83d4c422b2c2a59ab72715c801c78
Marshall <Evernight27@hotmail.com>
2023-04-06 15:44:19 -0400

    full crud with simplejwt, might switch to native django auth later

--- rabbit/models.py
+++ rabbit/models.py
@@ -1,20 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
-
-
-class User(models.Model):
-    username = models.CharField()
 
 
 class Burrow(models.Model):
     name = models.CharField()
-    total_carrots = models.IntegerField()
 
 
 class Post(models.Model):
     user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='posts')
+        User, on_delete=models.CASCADE, related_name='posts', null=True)
+    burrow = models.ForeignKey(
+        Burrow, on_delete=models.CASCADE, related_name='posts', null=True)
     title = models.CharField()
     content = models.TextField()
     carrots = models.IntegerField()
@@ -27,3 +24,8 @@ class Comment(models.Model):
         Post, on_delete=models.CASCADE, related_name='comments')
     content = models.TextField()
     carrots = models.IntegerField()
+
+
+class TotalCarrots(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='total_carrots', null=True)
