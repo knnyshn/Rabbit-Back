3198ab945a1ef281b8912b652aefddba33efc51e
Marshall <Evernight27@hotmail.com>
2023-04-10 09:30:27 -0400

    fixed id instead of name maybe

--- rabbit/serializers.py
+++ rabbit/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import Burrow, Post, Comment, Profile
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
 
 
 class ProfileSerializer(serializers.ModelSerializer):
@@ -28,10 +30,25 @@ class CommentSerializer(serializers.ModelSerializer):
         model = Comment
         fields = '__all__'
 
+    def to_representation(self, instance):
+        response = super().to_representation(instance)
+        response['user'] = UserSerializer(instance.user.all(), many=True).data
+        response['post'] = PostSerializer(instance.user.all(), many=True).data
+
+        return response
+
 
 class PostSerializer(serializers.ModelSerializer):
+
     comments = CommentSerializer(read_only=True, many=True)
 
     class Meta:
         model = Post
         fields = '__all__'
+
+    def to_representation(self, instance):
+        response = super().to_representation(instance)
+        response['user'] = UserSerializer(instance.user.all(), many=True)
+        response['burrow'] = BurrowSerializer(instance.burrow.all(), many=True)
+
+        return response
