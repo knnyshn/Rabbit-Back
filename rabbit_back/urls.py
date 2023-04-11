dca0424ed8d3fb7ec475b4c883640caf4e0147f4
Marshall <Evernight27@hotmail.com>
2023-04-11 00:50:04 -0400

    changed login api

--- rabbit_back/urls.py
+++ rabbit_back/urls.py
@@ -21,7 +21,8 @@ urlpatterns = [
 
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/auth/login/', TokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     # path('api-auth/', include('rest_framework.urls')),
